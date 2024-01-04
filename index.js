74d54aa610f5744a78d69aa70343e37a01720f4d
Klif <sang.wicklife@student.moringaschool.com>
2024-01-04 16:42:16 +0300

    Assignments completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers,dName) {
+    return drivers.filter(name=>name.toLowerCase()==dName.toLowerCase()?true:false);
+}
+
+function fuzzyMatch(drivers,letters) {
+    const strLen = letters.length;
+    return drivers.filter(name=>name.substr(0,strLen)==letters?true:false);
+}
+
+function matchName(drivers,dName) {
+    return drivers.filter(driver=>driver.name==dName?true:false);
+}
